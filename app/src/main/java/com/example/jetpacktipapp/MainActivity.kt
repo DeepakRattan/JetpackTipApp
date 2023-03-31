8efa798d4273623773617572e924d245c4d17d96
Deepak Rattan <deepak.rattan@itechnolabs.tech>
2023-03-31 12:24:03 +0530

    Implements top header composable function

--- app/src/main/java/com/example/jetpacktipapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktipapp/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.jetpacktipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,9 +11,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpacktipapp.ui.theme.JetpackTipAppTheme
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text("Hello")
+                TopHeader()
             }
         }
     }
@@ -36,7 +36,7 @@ fun MyApp(content: @Composable () -> Unit) {
     JetpackTipAppTheme {
         // A surface container using the 'background' color from the theme
         Surface(
-            modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+            color = MaterialTheme.colors.background
         ) {
             content()
         }
@@ -45,17 +45,35 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 130.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
             .clip(
                 shape = CircleShape.copy(all = CornerSize(12.dp))
-            )
+            ),
+        color = Color(0xFFE9D7f7)
 //            .clip(RoundedCornerShape(corner = CornerSize(12.dp)))
-        , color = Color.Magenta
     ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(all = 12.dp)
+
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
 
     }
 }
@@ -65,7 +83,7 @@ fun TopHeader() {
 fun DefaultPreview() {
     JetpackTipAppTheme {
         MyApp {
-            Text("Hello")
+            TopHeader()
         }
     }
 }
